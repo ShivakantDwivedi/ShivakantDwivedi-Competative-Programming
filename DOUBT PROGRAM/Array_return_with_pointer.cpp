d9d71949dc287feef365e92b86614e0fb1870bea
ShivakantDwivedi <itshivakant1472002>
2022-09-23 18:32:36 +0530

    Competative Programming

---
+++ DOUBT PROGRAM/Array_return_with_pointer.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+    int *sum()
+    {
+        int arr[10]={1,2,3,4,5};
+        for(int i=0;i<6;i++)
+        {
+            return arr;
+        }
+
+    }
+
+
+int main(){
+  
+
+    int *ptr=sum();
+    return 0;
+}
---
+++ DOUBT PROGRAM/Pointer_to_address.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int arr[100]={1,2,3,4};
+    cout<<*(arr+6);
+    cout<<(*arr+1);
+}
