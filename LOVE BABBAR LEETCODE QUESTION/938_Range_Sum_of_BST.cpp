6176ee19c595dc83fd726c527f27ca87e0e68293
ShivakantDwivedi <itshivakant1472002@gmail.com>
2023-03-29 01:03:51 +0530

    Competative programming

--- LOVE BABBAR LEETCODE QUESTION/938_Range_Sum_of_BST.cpp
+++ LOVE BABBAR LEETCODE QUESTION/938_Range_Sum_of_BST.cpp
@@ -33,3 +33,27 @@ All Node.val are unique.
 
 
 */
+
+
+
+Approch 1
+
+
+class Solution {
+public:
+    int sum=0;
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        
+        if(root==NULL)
+        {
+            return sum;
+        }
+        if(low<=root->val  && root->val<=high)
+        {
+            sum=sum+root->val;
+        }
+        rangeSumBST(root->left,low,high);
+        rangeSumBST(root->right,low,high);
+        return sum;
+    }
+};
