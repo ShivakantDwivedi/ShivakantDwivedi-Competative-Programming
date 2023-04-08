0ccbef95c1f9535e9ceb5e21c07314de4cef8bb1
ShivakantDwivedi <itshivakant1472002@gmail.com>
2023-04-08 09:13:53 +0530

    competativeProgramming

---
+++ CODECHEF CONTEST/Chef__and_Donation.cpp
@@ -0,0 +1,152 @@
+/*
+
+
+Problem
+In a certain month, Chef earned 
+�
+X rupees while Chefina earned 
+�
+Y rupees such that 
+�
+>
+�
+Y>X.
+Since they want to end up with exactly the same amount, they decide to donate the difference between their income to a charity.
+
+Find the amount they donate in the month.
+
+Input Format
+The first line of input will contain a single integer 
+�
+T, denoting the number of test cases.
+Each test case consists of two space-separated integers 
+�
+X and 
+�
+Y — the income of Chef and Chefina in a month, respectively.
+Output Format
+For each test case, output on a new line, the amount they donate in the month.
+
+Constraints
+1
+≤
+�
+≤
+100
+1≤T≤100
+1
+≤
+�
+<
+�
+≤
+10
+1≤X<Y≤10
+Sample 1:
+Input
+Output
+4
+1 3
+2 5
+4 5
+2 10
+2
+3
+1
+8
+Explanation:
+Test case 
+1
+1: Chef earns 
+1
+1 rupees while Chefina earns 
+3
+3 rupees. The difference between their income is 
+3
+−
+1
+=
+2
+3−1=2. Thus, they donate 
+2
+2 rupees to charity.
+
+Test case 
+2
+2: Chef earns 
+2
+2 rupees while Chefina earns 
+5
+5 rupees. The difference between their income is 
+5
+−
+2
+=
+3
+5−2=3. Thus, they donate 
+3
+3 rupees to charity.
+
+Test case 
+3
+3: Chef earns 
+4
+4 rupees while Chefina earns 
+5
+5 rupees. The difference between their income is 
+5
+−
+4
+=
+1
+5−4=1. Thus, they donate 
+1
+1 rupees to charity.
+
+Test case 
+4
+4: Chef earns 
+2
+2 rupees while Chefina earns 
+10
+10 rupees. The difference between their income is 
+10
+−
+2
+=
+8
+10−2=8. Thus, they donate 
+8
+8 rupees to charity.
+
+accepted
+Accepted
+14702
+total-Submissions
+Submissions
+16969
+accuracy
+Accuracy
+91.04
+
+
+
+
+
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x,y;
+	    cin>>x>>y;
+	    cout<<abs(x-y)<<endl;
+	}
+	return 0;
+}
