8b8bd0f679161290645b82e595d1f8db893699d5
ShivakantDwivedi <itshivakant1472002@gmail.com>
2023-04-24 22:02:42 +0530

    codding

--- RECURSION/141_Linked_list_cycle.cpp
+++ RECURSION/141_Linked_list_cycle.cpp
@@ -51,3 +51,31 @@ Acceptance Rate
 
 
 */
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        
+        if(head==NULL || head->next==NULL)
+        {
+            return false;
+        }
+
+        ListNode * fast=head;
+        ListNode *slow=head;
+
+        while(fast->next && fast->next->next)
+        {
+            fast=fast->next->next;
+            slow=slow->next;
+
+            if(slow==fast)
+            {
+                return true;
+            }
+        }
+        return false;
+
+
+    }
+};
