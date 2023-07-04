b888f7c16f7502907363d967a30cee845ef78bc7
ShivakantDwivedi <itshivakant1472002@gmail.com>
2023-07-04 22:38:49 +0530

    codding

---
+++ STRIVER SDE SHEET/DAY 1/Kadanes_Algo_maximum_sum_subarray.cpp
@@ -0,0 +1,110 @@
+/*
+
+December 14, 2021 Arrays / Data Structure
+Kadane’s Algorithm : Maximum Subarray Sum in an Array
+Problem Statement: Given an integer array arr, find the contiguous subarray (containing at least one number) which
+has the largest sum and returns its sum and prints the subarray.
+
+Examples
+Example 1:
+
+Input: arr = [-2,1,-3,4,-1,2,1,-5,4] 
+
+Output: 6 
+
+Explanation: [4,-1,2,1] has the largest sum = 6. 
+
+Examples 2: 
+
+Input: arr = [1] 
+
+Output: 1 
+
+Explanation: Array has only one element and which is giving positive sum of 1. 
+
+
+
+
+*/
+
+
+Brute Force
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxSubarraySum(int arr[], int n) {
+    int maxi = INT_MIN; // maximum sum
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i; j < n; j++) {
+            // subarray = arr[i.....j]
+            int sum = 0;
+
+            //add all the elements of subarray:
+            for (int k = i; k <= j; k++) {
+                sum += arr[k];
+            }
+
+            maxi = max(maxi, sum);
+        }
+    }
+
+    return maxi;
+}
+
+int main()
+{
+    int arr[] = { -2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int maxSum = maxSubarraySum(arr, n);
+    cout << "The maximum subarray sum is: " << maxSum << endl;
+    return 0;
+}
+
+
+
+
+Optimal Approch 
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+long long maxSubarraySum(int arr[], int n) {
+    long long maxi = LONG_MIN; // maximum sum
+    long long sum = 0;
+
+    for (int i = 0; i < n; i++) {
+
+        sum += arr[i];
+
+        if (sum > maxi) {
+            maxi = sum;
+        }
+
+        // If sum < 0: discard the sum calculated
+        if (sum < 0) {
+            sum = 0;
+        }
+    }
+
+    // To consider the sum of the empty subarray
+    // uncomment the following check:
+
+    //if (maxi < 0) maxi = 0;
+
+    return maxi;
+}
+
+int main()
+{
+    int arr[] = { -2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    long long maxSum = maxSubarraySum(arr, n);
+    cout << "The maximum subarray sum is: " << maxSum << endl;
+    return 0;
+}
+
